9e7d808ec3c115b1a7c120063b1faadc5fb28ad6
Evgeniy <Puchok13@yandex.ru>
2024-05-26 19:02:35 +0500

    Исправление замечаний по домашке 3

--- homework_3.js
+++ homework_3.js
@@ -1,28 +1,21 @@
 // TASK-1. Работа с переменными
 
-let item_1; // Объвили переменную
-
-item_1 = 5; //Присвоили значение переменной
-
+const item_1 = 5; 
 console.log(item_1); //Вывели значение переменной в консоль
 
-let item_2 = 3; //Объявили переменную с присвоением значения
-
+const item_2 = 3; //Объявили переменную с присвоением значения
 console.log(item_2); //вывели в консоль item_2
 
-let item_3;                 //Объявили переменную
-item_3 = item_1 + item_2;   //Присвоили переменной значение 
+const item_3 = item_1 + item_2;                 
 console.log(item_3);        //вывели в консоль item_3
 
-let item_4;                 //Объявили переменную
-item_4 = 'Yolochka';        //Присвоили переменной значение
+const item_4 = 'Yolochka';                 //Объявили переменную
 console.log(item_4);        //вывели в консоль item_4
 
 console.log(item_3 + item_4);   //Вывели в консоль сложение 2х переменных
 console.log(item_3 * item_4);   //Вывели в консоль умножение 2х переменных
 
-let item_5; //Объявили переменную
-item_5 = item_3; //Присвоили одну переменную другой
+const item_5 = item_3; //Объявили переменную
 
 let item_6; //Объявили переменную
 
@@ -34,7 +27,7 @@ item_6_type = item_6; //Присвоили тип переменной??? Тип
 
 console.log('item_6 == ' + typeof item_6, 'item_6_type == ' + typeof item_6_type); //Вывели в консоль типы данных
 
-let item_7 = String(item_6); //Обхявили переменную, присвоили значение другой переменно, изменили тип намбер на стринг
+const item_7 = String(item_6); //Обхявили переменную, присвоили значение другой переменно, изменили тип намбер на стринг
 //console.log(typeof item_7);
 
 let item_7_type;
@@ -69,8 +62,7 @@ console.log('Ответ к уравнению 2: ' + x1 + ' и ' + x2);
 
 //TASK-3 n+nn+nnn
 
-let n;
-n = 3;
+let n = 3;
 let summ_3n = '' + n + n + n;
 let summ_2n = '' + n + n;
 let summ_n = Number(summ_3n) + Number(summ_2n) + n;
